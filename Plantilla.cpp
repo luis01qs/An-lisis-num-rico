b14e0fdd2d59585487e0193be0b36502e76e6d0a
luis01qs <105955279+luis01qs@users.noreply.github.com>
2024-02-06 17:20:42 -0600

    Plantilla.cpp

--- Plantilla.cpp
+++ Plantilla.cpp
@@ -1,1 +1,17 @@
-#
+# include <bits/stdc++.h>
+# define ll long long
+# define nl '\n'
+
+using namespace std;
+
+int main()
+{
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  cout.tie(0);
+  freopen("prueba.txt", "r", stdin);
+
+  
+
+  return 0;
+}
